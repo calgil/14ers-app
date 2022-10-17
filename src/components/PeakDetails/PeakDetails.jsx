8f77bebcaf70f3c34f514c4131f4753243aa352d
calgil <clgilbert1105@gmail.com>
2022-10-17 10:13:16 -0600

    update peak details style

--- src/components/PeakDetails/PeakDetails.jsx
+++ src/components/PeakDetails/PeakDetails.jsx
@@ -11,7 +11,14 @@ import { UserContext } from "../../App";
 import AddPhoto from "../Admin/AddPhoto/AddPhoto";
 import elevation from "../../assets/PeakDetails/elevation.svg";
 import rank from "../../assets/PeakDetails/rank.svg";
-import range from "../../assets/PeakDetails/range.svg"
+import range from "../../assets/PeakDetails/range.svg";
+import addPeak from "../../assets/PeakDetails/addPeak.svg";
+import class1 from "../../assets/PeakDetails/class1.png";
+import class2 from "../../assets/PeakDetails/class2.png";
+import class2Plus from "../../assets/PeakDetails/class2Plus.png";
+import class3 from "../../assets/PeakDetails/class3.png";
+import class4 from "../../assets/PeakDetails/class4.png";
+import class5 from "../../assets/PeakDetails/class5.png";
 
 const PeakDetails = () => {
 
@@ -36,6 +43,26 @@ const PeakDetails = () => {
             })
     }, [id]);
 
+    const addDifficultyIcon = (str) => {
+        switch (str) {
+            case 'class 1':
+                return class1;
+            case 'class 2':
+                return class2;
+            case 'class 2+':
+                return class2Plus;
+            case 'class 3':
+                return class3;
+            case 'class 4':
+                return class4;
+            case 'class 5':
+                return class5;
+            default:
+                break;
+        }
+        return str;
+    }
+
     // const editPeak = () => {
     //     // navigate(`/peaks/edit/${id}`);
     //     setAddPhoto(true)
@@ -51,11 +78,12 @@ const PeakDetails = () => {
                 <div className={s.peakDetails}>
                     <div className={s.addBtnBar}>
                         <h3 className={s.peakName}>{peak.name}</h3>
-                        <button
-                            className={s.addToClimbsBtn}
-                        >
-                            Add to My Climbs
-                        </button>
+                        <div className={s.addToClimbsBtn}>
+                            <div className={s.iconContainer}>
+                                <img src={addPeak} alt="add peak" />
+                            </div>
+                            <span>Add to My Climbs</span>
+                        </div>
                     </div>
                     <div 
                         className={s.mainInfo}
@@ -82,19 +110,19 @@ const PeakDetails = () => {
                             className={s.statsContainer}
                         >
                             <div className={s.stat}>
-                                <div className={s.statIconContainer}>
+                                <div className={s.iconContainer}>
                                     <img src={elevation} alt="elevation" />
                                 </div>
                                 <span>Elevation: {peak.elevation} ft</span>
                             </div>
                             <div className={s.stat}>
-                                <div className={s.statIconContainer}>
+                                <div className={s.iconContainer}>
                                     <img src={rank} alt="rank" />
                                 </div>
                                 <span>Rank: {peak.rank}</span>
                             </div>
                             <div className={s.stat}>
-                                <div className={s.statIconContainer}>
+                                <div className={s.iconContainer}>
                                     <img src={range} alt="range" />
                                 </div>
                                 <span>Range: {peak.range}</span>
@@ -103,48 +131,50 @@ const PeakDetails = () => {
                     </div>
                     <div className={s.routesContainer}>
                         {
-                            peak.routes && 
-                            <>
-                                <h3>Routes</h3>
-                                <div className={s.routeTable}>
-                                    <div className={`${s.routeHeader} ${s.route}`}>
+                        peak.routes && 
+                            <div className={s.routeTable}>
+                                <div className={`${s.routeHeader} ${s.route}`}>
+                                    <div className={s.routeName}>
+                                        Name
+                                    </div>
+                                    <div className={s.routeStat}>
+                                        Mileage
+                                    </div>
+                                    <div className={s.routeStat}>
+                                        Gain
+                                    </div>
+                                    <div className={s.routeStat}>
+                                        Difficulty
+                                    </div>
+                                </div>
+                                {peak.routes.map((route) => (
+                                    <div 
+                                        key={route._id}
+                                        className={s.route}
+                                    >
                                         <div className={s.routeName}>
-                                            Name
+                                            {capitalizeFirstLetters(route.name)}
                                         </div>
                                         <div className={s.routeStat}>
-                                            Mileage
+                                            {route.mileage} miles
                                         </div>
                                         <div className={s.routeStat}>
-                                            Gain
+                                            {route.gain}'
                                         </div>
                                         <div className={s.routeStat}>
-                                            Difficulty
+                                            <div className={s.routeDifficultyContainer}>
+                                                <img src={addDifficultyIcon(route.difficulty)} alt="difficulty" />
+                                            </div>
                                         </div>
                                     </div>
-                                    {peak.routes.map((route) => (
-                                        <div className={s.route}>
-                                            <div className={s.routeName}>
-                                                {capitalizeFirstLetters(route.name)}
-                                            </div>
-                                            <div className={s.routeStat}>
-                                                {route.mileage} miles
-                                            </div>
-                                            <div className={s.routeStat}>
-                                                {route.gain}'
-                                            </div>
-                                            <div className={s.routeStat}>
-                                                {route.difficulty}
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div>
-                            </>
+                                ))}
+                            </div>
                         }
                     </div>
                 </div>
             }
         </div>
-    )
-}
+    );
+};
 
 export default PeakDetails;
